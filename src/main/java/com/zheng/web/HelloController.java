b41891ebda2064b100beb7d698d6e8ea62492022
zl736732419 <736732419@qq.com>
2016-09-18 17:41:59 +0800

    删除HelloController中方法标记的@ResponseBody

--- src/main/java/com/zheng/web/HelloController.java
+++ src/main/java/com/zheng/web/HelloController.java
@@ -3,13 +3,18 @@ package com.zheng.web;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.zheng.extensions.ProfilePropertySourceBean;
 import com.zheng.extensions.ValuePropertySourceBean;
 import com.zheng.extensions.YamlPropertySourceBean;
-
+/**
+ * 直接使用@RestController标识请求返回的是json格式数据
+ * 所以我们无需再添加@ResponseBody注解标签
+ *
+ * @author zhenglian
+ * @data 2016年9月18日 下午5:40:20
+ */
 @RestController
 public class HelloController {
 
@@ -23,25 +28,21 @@ public class HelloController {
 	private YamlPropertySourceBean yamlValue;
 
 	@RequestMapping(value = "/hello", method = RequestMethod.GET)
-	@ResponseBody
 	public String hello() {
 		return "helloworld";
 	}
 
 	@RequestMapping("/value")
-	@ResponseBody
 	public String valueProperty() {
 		return value.getName() + "," + value.getAge() + value.getServers();
 	}
 
 	@RequestMapping("/yaml_value")
-	@ResponseBody
 	public String yamlValueProperty() {
 		return yamlValue.getName() + "," + yamlValue.getAge() + yamlValue.getServers();
 	}
 	
 	@RequestMapping("/profile_value")
-	@ResponseBody
 	public String profileValueProperty() {
 		return profileValue.getAddress();
 	}
